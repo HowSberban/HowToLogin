01b07c35fb4a842c27cfba53210af6b888124802
HowSberban <753657741@qq.com>
2026-07-27 13:20:06 +0800

    依赖库fix

--- src/main/java/org/HowToLogin/plugin/PluginLoaderImpl.java
+++ src/main/java/org/HowToLogin/plugin/PluginLoaderImpl.java
@@ -7,6 +7,7 @@ import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
 
+@SuppressWarnings({"unused", "UnstableApiUsage"})
 public final class PluginLoaderImpl implements PluginLoader {
 
     @Override
