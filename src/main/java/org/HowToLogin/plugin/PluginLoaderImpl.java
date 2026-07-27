62bb5b6b3e4fb278326473fd2baf9705b62b6d35
HowSberban <753657741@qq.com>
2026-07-27 12:19:47 +0800

    依赖库

---
+++ src/main/java/org/HowToLogin/plugin/PluginLoaderImpl.java
@@ -0,0 +1,26 @@
+package org.HowToLogin.plugin;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.repository.RemoteRepository;
+
+public final class PluginLoaderImpl implements PluginLoader {
+
+    @Override
+    public void classloader(PluginClasspathBuilder classpathBuilder) {
+        MavenLibraryResolver resolver = new MavenLibraryResolver();
+        resolver.addRepository(new RemoteRepository.Builder(
+                "central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
+
+        // 运行时依赖：数据库驱动、连接池、密码哈希库
+        resolver.addDependency(new Dependency(new DefaultArtifact("org.xerial:sqlite-jdbc:3.46.1.3"), null));
+        resolver.addDependency(new Dependency(new DefaultArtifact("com.mysql:mysql-connector-j:8.4.0"), null));
+        resolver.addDependency(new Dependency(new DefaultArtifact("com.zaxxer:HikariCP:5.1.0"), null));
+        resolver.addDependency(new Dependency(new DefaultArtifact("org.mindrot:jbcrypt:0.4"), null));
+
+        classpathBuilder.addLibrary(resolver);
+    }
+}
